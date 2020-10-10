3ec677e6ee4b726a13fd019e35d9f532d29fec0b
Devansh Tayal <devanshtayal2005@gmail.com>
2020-10-10 13:56:54 +0530

    Added Functionalities

--- Voice/1.py
+++ Voice/1.py
@@ -3,6 +3,8 @@ from speech_recognition import Recognizer
 from speech_recognition import Microphone
 from speech_recognition import RequestError
 from speech_recognition import UnknownValueError
+from time import sleep, ctime
+
 
 
 tts_engine =pyttsx3.init("sapi5")
@@ -32,13 +34,52 @@ def record_audio(ask=False):
             return audio_text.lower()
         except UnknownValueError:
             speak_electrica("Sorry I didn't get that. kindly speak up again")
+            say_again()
+
 
         except RequestError:
             speak_electrica("Sorry, unable to connect with engine")
 
 
+def search_command(audio_text):
+    #Devis Introduction
+    if "your name" in audio_text:
+        speak_electrica("my name is Devis.")
+        sleep(3)
+        something_else()
+
+        #Current time
+    elif "current time" in audio_text or "time" in audio_text:
+        speak_electrica(ctime())
+        sleep(3)
+        something_else()
+
+    elif "exit" in audio_text:
+        speak_electrica("Ok, Bye Bye")
+        exit()
+
+    
+    else:
+        speak_electrica("Sorry I didn't get that. kindly speak up again")
+        say_again()
+
+
+def say_again():
+    speak_electrica("Give me the command..")
+   
+    audio_text=record_audio()
+    search_command(audio_text)
+
+def something_else():
+    audio_text=record_audio("Do you want something else ? Yes or No")
+    if "yes" in audio_text:
+        say_again()
+    else:
+        speak_electrica("Ok, Bye Bye")
+        exit()
+
 
 
 
 speak_electrica("Hello, how can i help you")
-audio_text=record_audio()
+say_again()
